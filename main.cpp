c2f4e4feb4fe69685888d79307d29c21e2e74603
Lucas Karpinski <karplucas@gmail.com>
2022-11-13 13:14:19 -0500

    Added const string containing ubuntu json url

--- main.cpp
+++ main.cpp
@@ -1,5 +1,11 @@
+#include "HTTPDownloader.hpp"
 #include <iostream>
+#include <string>
 
-int main(int, char**) {
-    std::cout << "Hello, world!\n";
+const std::string ubuntu_url = "https://cloud-images.ubuntu.com/releases/streams/v1/com.ubuntu.cloud:released:download.json";
+
+int main(int argc, char** argv) {
+    HTTPDownloader downloader;
+    std::string content = downloader.download(ubuntu_url);
+    std::cout << content << std::endl;
 }
