92764c3c2415b152f3ad410457276b648ac77355
Lucas Karpinski <karplucas@gmail.com>
2022-11-14 16:08:46 -0500

    Removed commented code

---
+++ include/ubuntufetcher.hpp
@@ -0,0 +1,46 @@
+/**
+ * ubuntufetcher.hpp
+ *
+ * A header defining a class for storing / accessing Ubuntu Cloud image information.
+ *
+ */
+#ifndef UBUNTU_FETCHER_HPP
+#define UBUNTU_FETCHER_HPP
+
+#include <string>
+#include <nlohmann/json.hpp>
+
+using json = nlohmann::json;
+
+
+ /**
+  * An Ubuntu information fetcher class.
+  */
+class UbuntuFetcher {
+public:
+    UbuntuFetcher();
+    ~UbuntuFetcher();
+    /**
+    * Download the Simplestream data to std::string info.
+    */
+    void download();
+    /**
+    * Return all AMD64 Ubuntu supported releases.
+    */
+    void supportedReleases();
+    /**
+    * Return the current LTS Ubuntu version.
+    */
+    void currentVersion();
+    /**
+    * Return the SHA256 for disk1.img provided.
+    */
+    void UbuntuFetcher::imageSHA256(const std::string& name);
+
+private:
+    const std::string url = "https://cloud-images.ubuntu.com/releases/streams/v1/com.ubuntu.cloud:released:download.json";
+    json releases;
+
+};
+
+#endif  /* UBUNTU_FETCHER_HPP */
